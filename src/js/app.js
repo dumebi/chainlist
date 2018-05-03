f815b7db6da6e43b74bd7bdbf5c7a7f65fe60226
tay.andrew <tay.andrew@yahoo.com>
2018-05-03 08:05:18 +0100

    serve over https 6

--- src/js/app.js
+++ src/js/app.js
@@ -18,7 +18,7 @@ App = {
             App.web3Provider = web3.currentProvider;
         } else {
             //create a new provider and plug it directly into our local node
-            App.web3Provider = new Web3.providers.HttpProvider('https://127.0.0.1:8080');
+            App.web3Provider = new Web3.providers.HttpProvider('https://159.89.119.189:8080');
             // App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
         }
         web3 = new Web3(App.web3Provider);
