9cc20ac19c4d0c80d1963a1fdce58c2733eb735b
tay.andrew <tay.andrew@yahoo.com>
2018-05-07 17:26:11 +0100

    adding server files

--- src/js/app.js
+++ src/js/app.js
@@ -78,7 +78,7 @@ App = {
     transfer: function() {
         web3.personal.unlockAccount(App.coinbase, "pass12345", 100000, function (err, result) {
             console.log(result)
-            consol.log(err)
+            console.log(err)
         web3.personal.unlockAccount(App.account, "pass@123", 100000, function (err, result) {
             web3.eth.sendTransaction({
                 from: App.coinbase,
