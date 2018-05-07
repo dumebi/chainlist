79f0ced3547025e8344c783a1779381fc25d3138
tay.andrew <tay.andrew@yahoo.com>
2018-05-07 22:02:08 +0100

    adding server files

--- src/js/app.js
+++ src/js/app.js
@@ -18,8 +18,8 @@ App = {
             App.web3Provider = web3.currentProvider;
         } else {
             //create a new provider and plug it directly into our local node
-            //App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
-            App.web3Provider = new Web3.providers.HttpProvider('http://159.89.119.189:8545');
+            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+            //App.web3Provider = new Web3.providers.HttpProvider('http://159.89.119.189:8545');
         }
         web3 = new Web3(App.web3Provider);
 
