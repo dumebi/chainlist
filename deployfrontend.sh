8ad6f8bad3c171d8d57171e54299d726eaa10f69
tay.andrew <tay.andrew@yahoo.com>
2018-05-14 12:28:33 +0100

    adding server files

--- deployfrontend.sh
+++ deployfrontend.sh
@@ -1,6 +1,6 @@
 #rsync -r src/ docs/
 rsync build/contracts/Chainlist.json src/
-rsync build/contracts/Chainlist.json .
+#rsync build/contracts/Chainlist.json .
 git add .
 git commit -m "adding server files"
 git push origin master
